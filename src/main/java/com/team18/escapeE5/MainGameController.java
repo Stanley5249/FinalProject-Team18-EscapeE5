695fc2c8a3d08b9e1950da3c4545975e3ae254a0
Stanley5249 <59500538+Stanley5249@users.noreply.github.com>
2022-05-15 15:23:47 +0800

    黃泓昌 05/15 15:23

--- src/main/java/com/team18/escapeE5/MainGameController.java
+++ src/main/java/com/team18/escapeE5/MainGameController.java
@@ -9,6 +9,6 @@ public class MainGameController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("MainGameController");
     }
 }
--- src/main/java/com/team18/escapeE5/SlidePuzzleController.java
+++ src/main/java/com/team18/escapeE5/SlidePuzzleController.java
@@ -7,7 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyEvent; 
 import javafx.scene.layout.GridPane;
 
 import java.util.Random;
