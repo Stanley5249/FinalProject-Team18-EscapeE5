d2e1ae3e3ea85ddb4505cf6d26b7149f67e8f0d2
Stanley5249 <59500538+Stanley5249@users.noreply.github.com>
2022-05-19 23:15:50 +0800

    黃泓昌 05/19 23:15

--- src/main/java/com/team18/escapeE5/footballController.java
+++ src/main/java/com/team18/escapeE5/footballController.java
@@ -2,12 +2,15 @@ package com.team18.escapeE5;
 
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+
+import java.util.Random;
 
 public class footballController extends AnimationTimer {
     @FXML
@@ -19,69 +22,110 @@ public class footballController extends AnimationTimer {
     private double ballRotate;
     private double dx = 0;
     private double dy = 0;
-    private double dr = 0;
 
-    private static final int spf = (int) (1.0 / 60 * 1e9);
-    private long animationStart;
+    @FXML
+    private Label score;
+
+    private int click;
 
     @Override
     public void handle(long now) {
-        if (now - animationStart > spf) {
-            animationStart = now;
-            dx *= 0.99;
-            dy *= 0.99;
-            dx = (ballX - radius <= 0 && dx < 0) || (ballX + radius >= ball.getScene().getWidth() && dx > 0) ? -dx : dx;
-            dy = (ballY - radius <= 0 && dy < 0) || (ballY + radius >= ball.getScene().getHeight() && dy > 0) ? 1 - dy : 1 + dy;
-            if (ballY + radius > ball.getScene().getHeight() && dy > 0) {
-                dx *= 0.99;
-                dy = 0;
-            }
-            dr = dx * 0.5;
-            moveBall();
+        if ((ballX - radius <= 0 && dx < 0) || (ballX + radius >= ball.getScene().getWidth() && dx > 0)) dx *= -0.8;
+        else dx *= 0.999;
+        if (ballY + radius >= ball.getScene().getHeight() && dy > 0) {
+            dy *= -0.8;
+            ball.setOnMouseClicked(null);
+            ball.getParent().setOnKeyPressed(this::waitReset);
         }
-    }
-
-    @FXML
-    protected void clickToStart(MouseEvent mouseEvent) {
-        start();
-        System.out.println("start");
-        onSoccerClick(mouseEvent);
-        ball.setOnMouseClicked(this::onSoccerClick);
-    }
-
-    @FXML
-    protected void onSoccerClick(MouseEvent mouseEvent) {
-        dx = Math.sin(mouseEvent.getX() / radius) * -15;
-        dy = (mouseEvent.getY() / radius + 9) * -2;
-        System.out.printf("%.1f %.1f\n", dx, dy);
-    }
-
-    @FXML
-    protected void onKeyPressed(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.SPACE) reset();
-    }
-
-    private void moveBall() {
+        else if (ballY - radius <= 0 && dy < 0) dy *= -0.8;
+        else dy *= 0.999;
+        if (ballY + radius < ball.getScene().getHeight()) {
+            dy += 0.4;
+        }
+        else dx *= 0.99;
         ballX += dx;
         ballY += dy;
-        ballRotate += dr;
+        ballRotate += dx * 0.45;
         ball.setLayoutX(ballX);
         ball.setLayoutY(ballY);
         ball.setRotate(ballRotate);
+        score.setText(String.valueOf(click));
+    }
+
+    private void clickToStart(MouseEvent mouseEvent) {
+        start();
+        onMouseClick(mouseEvent);
+        ball.setOnMouseClicked(this::onMouseClick);
+    }
+
+    private void onMouseClick(MouseEvent mouseEvent) {
+        double x = -Math.sin(mouseEvent.getX() / radius / 2 * Math.PI) * 6;
+        double y = -(mouseEvent.getY() / radius + 10);
+        setBallMomentum(x, y);
+    }
+
+    private void pressToStart(KeyEvent keyEvent) {
+        start();
+        onKeyPressed(keyEvent);
+        ball.setOnKeyPressed(this::onKeyPressed);
+    }
+
+    private void onKeyPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case W -> {
+                Random random = new Random();
+                double x = Math.sin(random.nextDouble(-0.1, 0.1) * Math.PI) * 6;
+                double y = random.nextDouble(-11, -10);
+                setBallMomentum(x, y);
+            }
+            case S -> {
+                Random random = new Random();
+                double x = Math.sin(random.nextDouble(-0.2, 0.2) * Math.PI) * 6;
+                double y = random.nextDouble(-10, -9);
+                setBallMomentum(x, y);
+            }
+            case A -> {
+                Random random = new Random();
+                double x = Math.sin(random.nextDouble(-0.5, -0.2) * Math.PI) * 6;
+                double y = random.nextDouble(-10.5, -9.5);
+                setBallMomentum(x, y);
+            }
+            case D -> {
+                Random random = new Random();
+                double x = Math.sin(random.nextDouble(0.2, 0.5) * Math.PI) * 6;
+                double y = random.nextDouble(-10.5, -9.5);
+                setBallMomentum(x, y);
+            }
+            case SPACE -> reset();
+        }
+    }
+
+    private void setBallMomentum(double x, double y) {
+        dx = x;
+        dy = y;
+        click ++;
+        System.out.printf("clicks: %d, (%4.1f, %4.1f)\n", click, dx, dy);
+    }
+
+    private void waitReset(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.SPACE) reset();
     }
 
     private void reset() {
         stop();
         ball.setOnMouseClicked(this::clickToStart);
+        ball.getParent().setOnKeyPressed(this::pressToStart);
         ballX = 150.0;
         ballY = 336.0;
         ballRotate = 0;
         dx = 0;
         dy = 0;
-        dr = 0;
         ball.setLayoutX(ballX);
         ball.setLayoutY(ballY);
         ball.setRotate(ballRotate);
+        click = 0;
+        score.setText(String.valueOf(click));
+        System.out.println("Reset");
     }
 
     @FXML
